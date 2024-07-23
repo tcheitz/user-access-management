5573bf542a2e57ef66d74928d778ec19ecfa59e7
Chaitanya <chaitanya.t.kumar@gmail.com>
2024-07-23 22:33:37 +0530

    initial commit

---
+++ src/main/java/com/example/user_access_management/UserAccessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_access_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccessManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_access_management/UserAccessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_access_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
