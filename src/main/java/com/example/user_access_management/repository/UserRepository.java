094fd36eba9ad516db0608680c862ddd5fe3e1b3
Chaitanya <chaitanya.t.kumar@gmail.com>
2024-07-23 23:05:07 +0530

    add repository

---
+++ src/main/java/com/example/user_access_management/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.user_access_management.repository;
+
+import com.example.user_access_management.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
